2b1148dcbf8cc73a9dcb48c51116f508a801168a
ExplosiveEnd <70591446+ExplosiveEnd@users.noreply.github.com>
2023-12-01 02:10:52 -0500

    Delete day-01/target directory

--- day-01/target/debug/part1.d
+++
@@ -1,1 +0,0 @@
-/home/explosiveend/projects/advent-of-code/2023/day-01/target/debug/part1: /home/explosiveend/projects/advent-of-code/2023/day-01/src/bin/input.txt /home/explosiveend/projects/advent-of-code/2023/day-01/src/bin/part1.rs
